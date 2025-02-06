a5b17d73db92c2fd52c3e077811498aa9ff3348b
camilolaiton <camilo.laiton@alleninstitute.org>
2025-02-06 05:17:53 +0000

    Including more metadata in the init file

--- code/aind_smartspim_fuse/__init__.py
+++ code/aind_smartspim_fuse/__init__.py
@@ -3,3 +3,21 @@ Module init file
 """
 
 __version__ = "0.0.2"
+__authors__ = ["Camilo Laiton"]
+__author_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+]
+__license__ = "MIT"
+__description__ = (
+    "Algorithm to fuse image stacks together "
+    "by using the transforms computed in the stitching step"
+)
+__url__ = "https://github.com/AllenNeuralDynamics/aind-smartspim-destripe"
+
+__maintainers__ = ["Camilo Laiton"]
+__maintainer_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+]
+__title__ = "aind-smartspim-destripe"
+__status__ = "Production"  # 'Development' 'Production', 'Beta'
+__pipeline_version__ = "3.0.0"
